adc1985539ca602d6d388f97ffebf0f9df46a164
kostyabek <kostyabek@gmail.com>
2022-04-17 15:40:22 +0300

    Added endpoint for setting profile info

--- Services/Users/Users.API/Controllers/ProfileController.cs
+++ Services/Users/Users.API/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
-﻿namespace Users.API.Controllers
+﻿using Users.Core.CQRS.Commands.Profile.SetProfileInfo;
+
+namespace Users.API.Controllers
 {
     using Core.CQRS.Commands.Profile.SetAvatar;
     using Core.Models.Request.Profile;
@@ -8,7 +10,7 @@
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
-    [Route("api/v1.0/users/{id:int}/profile")]
+    [Route("api/v1.0/profile")]
     [Authorize(Roles = "User, Admin")]
     public class ProfileController : Controller
     {
@@ -19,12 +21,33 @@
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Set profile avatar.
+        /// </summary>
+        /// <param name="request">Avatar data.</param>
+        /// <returns></returns>
         [HttpPost("avatar")]
-        public async Task<IActionResult> SetAvatarAsync(
-            int id,
-            [FromForm] SetAvatarRequest request)
+        public async Task<IActionResult> SetAvatarAsync([FromForm] SetAvatarRequest request)
         {
             var command = new SetAvatarCommand { Avatar = request.Avatar };
+            var result = await _mediator.Send(command);
+            return this.FromExecutionResult(result);
+        }
+
+        /// <summary>
+        /// Set profile info.
+        /// </summary>
+        /// <param name="request">Avatar data.</param>
+        /// <returns></returns>
+        [HttpPut("info")]
+        public async Task<IActionResult> SetAvatarAsync([FromBody] SetProfileInfoRequest request)
+        {
+            var command = new SetProfileInfoCommand
+            {
+                Bio = request.Bio,
+                DisplayName = request.DisplayName
+            };
+
             var result = await _mediator.Send(command);
             return this.FromExecutionResult(result);
         }
---
+++ Services/Users/Users.Core/CQRS/Commands/Profile/SetProfileInfo/SetProfileInfoCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using LS.Helpers.Hosting.API;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Users.Core.Database;
+using Users.Core.Services.User;
+
+namespace Users.Core.CQRS.Commands.Profile.SetProfileInfo;
+
+public class SetProfileInfoCommandHandler : IRequestHandler<SetProfileInfoCommand, ExecutionResult>
+{
+    private readonly UsersDbContext _dbContext;
+    private readonly IUserService _userService;
+
+    public SetProfileInfoCommandHandler(
+        UsersDbContext dbContext,
+        IUserService userService
+    )
+    {
+        _dbContext = dbContext;
+        _userService = userService;
+    }
+
+    public async Task<ExecutionResult> Handle(SetProfileInfoCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await _userService.GetCurrentUserAsync();
+
+            user.Bio = request.Bio;
+            user.DisplayName = request.DisplayName;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return new ExecutionResult(new InfoMessage("Profile info has been successfully updated."));
+        }
+        catch (Exception e)
+        {
+            return new ExecutionResult(
+                new ErrorInfo($"Error while executing SetProfileInfoCommand.\n> {e.Message}"));
+        }
+    }
+}
